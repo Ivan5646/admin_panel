e858cae041552057251e1bac8da5d205a998c9f7
Mik808 <Mike515@yandex.ru>
2020-02-14 13:21:36 +0700

    item page

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ var uglify = require('gulp-uglify');
 var replace = require('gulp-replace');
 var imagemin = require('gulp-imagemin');
 
-var htmlFile = "template";
+var htmlFile = "item";
 
 gulp.task('connect', function() {
     connect.server({
